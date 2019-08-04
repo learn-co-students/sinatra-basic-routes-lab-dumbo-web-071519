341f63d51a836f36462540651a93ac8b968f37d8
Janet Mendez <janetmendez.03@gmail.com>
2019-08-04 02:13:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Janet Mendez"
+    end
+    get '/hometown' do
+        "My hometown is Queens, New York"
+    end
+    get '/favorite-song' do
+        "My favorite song is currently Colors"
+    end
+
 end
