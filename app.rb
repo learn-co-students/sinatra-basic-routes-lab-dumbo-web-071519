37fdda3e20dfbf6e9279d03974408379611c479c
Alessandra Vertrees <avertrees447@gmail.com>
2019-08-04 13:22:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Alessandra"
+    end
+    get '/hometown' do
+        "My hometown is Narrowsburg, New York"
+    end
+    get '/favorite-song' do
+        "My favorite song is Purple Rain by Prince"
+    end
 end
